3590b9599c649412ec9109266dd91bf1227606b4
enroig2 <roigtormo.1999@gmail.com>
2023-06-28 17:06:30 +0200

    newinfo

--- Projects/Prueba_QC_Programs_Optimize/trf_output_fix_PAIRED_name.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for f in *.fastq; do mv "$f" "$(echo "$f" | sed s/.denovo_duplicates_marked.trimmed./_denovo_duplicates_marked_trimmed_/)"; done
--- Projects/Prueba_QC_Programs_Optimize/trimmomatic_output_fix_PAIRED_name.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for f in *.fastq; do mv "$f" "$(echo "$f" | sed s/kneaddata.trimmed/kneaddata_trimmed/)"; done
